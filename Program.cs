93ab499a6dfe137552e060a65d2b78597dbc6b00
AdamRB208 <adamrborup@gmail.com>
2025-05-15 11:15:39 -0600

    set conditional and logging user input.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
     Console.Clear();
     Console.WriteLine("Rock,Paper,Scissors");
     string userHand = ChooseHand();
+    Console.WriteLine(userHand);
   }
   static string ChooseHand()
   {
@@ -13,7 +14,24 @@
     Console.WriteLine("2. Paper");
     Console.WriteLine("3. Scissors");
     string userInput = Console.ReadLine();
+    if (userInput != "1" && userInput != "2" && userInput != "3")
+    {
+      Console.WriteLine("Invalid choice. Please choose 1, 2, or 3.");
+      return ChooseHand();
+    }
+    switch (userInput)
+    {
+      case "1":
+        userInput = "Rock";
+        break;
+      case "2":
+        userInput = "Paper";
+        break;
+      case "3":
+        userInput = "Scissors";
+        break;
+    }
 
-    return "";
+    return "You have chosen " + userInput;
   }
 }
