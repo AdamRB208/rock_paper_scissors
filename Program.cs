11d44c31d1ce95215d4f02803f02e629c6f626de
AdamRB208 <adamrborup@gmail.com>
2025-05-15 11:05:40 -0600

    created choose hand method

--- Program.cs
+++ Program.cs
@@ -2,7 +2,18 @@
 {
   private static void Main()
   {
-    Console.WriteLine("Main is running");
+    Console.Clear();
     Console.WriteLine("Rock,Paper,Scissors");
+    string userHand = ChooseHand();
+  }
+  static string ChooseHand()
+  {
+    Console.WriteLine("Choose a hand");
+    Console.WriteLine("1. Rock");
+    Console.WriteLine("2. Paper");
+    Console.WriteLine("3. Scissors");
+    string userInput = Console.ReadLine();
+
+    return "";
   }
 }
